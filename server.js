ffcb26b25cfd610209c9d10a4faf0a29759eafa8
fredzica <fredzica@gmail.com>
2024-09-30 09:37:02 -0300

    bcrypt refresher

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+const bcrypt      = require('bcrypt');
+
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
@@ -11,13 +13,20 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => {
+        console.log(res);
+    })
+})
 
 //END_ASYNC
 
 //START_SYNC
 
-
+const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+const result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 //END_SYNC
 
